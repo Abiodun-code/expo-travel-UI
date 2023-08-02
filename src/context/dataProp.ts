80ac782bbcd45c4b37c63427f0cedd8a17ffd694
Abiodun-code <olaabiodun157@gmail.com>
2023-08-02 00:39:36 -0700

    expo travel UI first commit

---
+++ src/context/dataProp.ts
@@ -0,0 +1,15 @@
+export interface categoryInterface {
+  title: string;
+  image: any;
+}
+
+export interface destinationInterface {
+  title: string;
+  duration: string;
+  distance: string;
+  weather: string;
+  price: number;
+  shortDescription: string;
+  longDescription: string;
+  image: any;
+}
